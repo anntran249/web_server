6c248d88f09bc55e412641d5935c6d69085f2deb
Isaac Boehman <idboehman@gmail.com>
2015-03-05 18:14:09 -0500

    Adds work from figuring out how to parse request

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,50 @@ fn main() {
     }
     drop(acceptor);
 }
+
+// structure of request (only handling GETs)
+// GET <path to file> HTTP
+// <path to file> is of unknown length
+//  - max length of 8192 - 9 = 8183
+//  - 9 from 4 bytes from "GET " + 5 bytes from " HTTP"
     
 fn handle_client(mut stream: TcpStream) {
     let mut buf = [0; MAX_REQUEST_LENGTH];
     stream.read(&mut buf);
+    let get_request = |&:| { buf.starts_with("GET ".as_bytes()) };
+
     if DEBUG {
         println!("Got request: {}", std::str::from_utf8(&buf).unwrap());
+
+        println!("starts_with 'GET ': {}", buf.starts_with("GET ".as_bytes()));
+
+        println!("get_request : {}", get_request());
+
+        let request: &[u8]  = buf.splitn(1, |&: x| *x == 0).next().unwrap();
+        let request_str: &str = std::str::from_utf8(request).unwrap();
+        let new_lines: &[_] = &['\n', '\r'];
+        let trimmed: &str = request_str.trim_right_matches(new_lines);
+        let mut splits = trimmed.split(' ');
+        if splits.clone().count() == 3 {
+            let method: &str = splits.next().unwrap();
+            let file: &str = splits.next().unwrap();
+            let ending: &str = splits.next().unwrap();
+            println!("method: {}", method);
+            println!("file: {}", file);
+            println!("ending: {}", ending);
+            println!("{}", ending.trim_matches(|&: c: char| c == '/' || c == '.' || c.is_numeric()));
+        } else {
+            let method: &str = "";
+            let file: &str = "";
+            let ending: &str = "";
+        }
+
+        println!("request: {:?}", request_str);
+        println!("request2: {:?}", trimmed);
+        println!("ends_with ' HTTP': {}", trimmed.ends_with(" HTTP"));
+        println!("\n")
     }
+
     match &buf[0..4] {
         b"GET " => {
             let resp: &str = "HTTP/1.0 200 OK\nweb_server\nContent-type: text/plain\nContent-Length: 2\n\nOK\n";
